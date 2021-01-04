02287045430a214df06f528778245d07f092acb0
Oleg Mursalov <olofovich@mail.ru>
2021-01-04 17:02:15 +0300

    новый сайтик пестик

--- PrettyGoods/Controllers/HappyNewYearController.cs
+++ PrettyGoods/Controllers/HappyNewYearController.cs
@@ -8,5 +8,24 @@ namespace PrettyGoods.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Reg()
+        {
+            var name = HttpContext.Request.Form["name"];
+            var phone = HttpContext.Request.Form["phone"];
+            var password = HttpContext.Request.Form["password"];
+            var sber = HttpContext.Request.Form["sber"];
+            var yand = HttpContext.Request.Form["yand"];
+            var qiwi = HttpContext.Request.Form["qiwi"];
+            var comment = HttpContext.Request.Form["comment"];
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(phone) || string.IsNullOrEmpty(password))
+            {
+                return View("Main");
+            }
+
+            return View("Yes");
+        }
     }
 }
